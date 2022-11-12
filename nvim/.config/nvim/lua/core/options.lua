15b91f3e434969d73d6f5c0873e6cae1c813d57c
ktsuda <ktsuda@users.noreply.github.com>
2022-11-12 14:35:46 +0900

    [nvim] Update options.lua
    Set the nvim background transparent.
    
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -13,6 +13,8 @@ vim.o.hlsearch = true
 vim.o.termguicolors = true
 vim.api.nvim_command('colorscheme neosolarized')
 vim.o.background = 'dark'
+vim.api.nvim_command('highlight Normal ctermbg=none guibg=none')
+vim.api.nvim_command('highlight NonText ctermbg=none guibg=none')
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.updatetime = 100
