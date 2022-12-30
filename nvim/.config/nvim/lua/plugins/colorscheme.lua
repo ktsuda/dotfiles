f0cb051c0b63b494d111ace29b8044e44ae5d7bb
ktsuda <ktsuda@users.noreply.github.com>
2022-12-30 15:37:08 +0900

    [nvim] Fix bug
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,4 @@
--- vim.api.nvim_command('colorscheme NeoSolarized')
-local color_status, _ = pcall(vim.api.nvim_command, 'colorscheme Neosolarized')
+local color_status, _ = pcall(vim.api.nvim_command, 'colorscheme NeoSolarized')
 if not color_status then return end
 vim.o.background = 'dark'
 vim.api.nvim_command('highlight Normal ctermbg=none guibg=none')
