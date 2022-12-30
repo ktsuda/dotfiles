b1ce2f175ddfb7895da9112e68b7a1afc72fd85a
ktsuda <ktsuda@users.noreply.github.com>
2022-12-30 22:01:29 +0900

    [wezterm] Update config file
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require('wezterm')
 return {
-  font_size = 14.0,
+  font_size = 13.0,
   color_scheme = 'Solarized Dark - Patched',
   leader = { key = 't', mods = 'CTRL', timeout_milliseconds = 1000 },
   keys = {
@@ -24,6 +24,10 @@ return {
     { key = 'k', mods = 'LEADER|SHIFT', action = wezterm.action.AdjustPaneSize{ 'Up', 1 } },
     { key = 'c', mods = 'LEADER', action = wezterm.action.SpawnTab 'CurrentPaneDomain' },
     { key = 'c', mods = 'LEADER|CTRL', action = wezterm.action.SpawnTab 'CurrentPaneDomain' },
+    { key = 'n', mods = 'LEADER', action = wezterm.action.ActivateTabRelative(1) },
+    { key = 'n', mods = 'LEADER|CTRL', action = wezterm.action.ActivateTabRelative(1) },
+    { key = 'p', mods = 'LEADER', action = wezterm.action.ActivateTabRelative(-1) },
+    { key = 'p', mods = 'LEADER|CTRL', action = wezterm.action.ActivateTabRelative(-1) },
     { key = 'q', mods = 'LEADER', action = wezterm.action.TogglePaneZoomState },
     { key = 'q', mods = 'LEADER|CTRL', action = wezterm.action.TogglePaneZoomState },
     { key = 'Enter', mods = 'CMD', action = wezterm.action.ToggleFullScreen },
