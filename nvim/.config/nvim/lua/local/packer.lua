eb1fd267b1e492e15fef8aa23236efaf64409248
ktsuda <ktsuda@users.noreply.github.com>
2023-02-08 15:41:53 +0900

    [nvim] Install cmp-nvim-lua plugin
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- nvim/.config/nvim/lua/local/packer.lua
+++ nvim/.config/nvim/lua/local/packer.lua
@@ -88,6 +88,7 @@ packer.startup(function(use)
     { 'hrsh7th/cmp-nvim-lsp' },
     { 'L3MON4D3/LuaSnip' },
     { 'saadparwaiz1/cmp_luasnip' },
+    { 'hrsh7th/cmp-nvim-lua' },
     { 'rafamadriz/friendly-snippets' },
     { 'jose-elias-alvarez/null-ls.nvim' },
   })
