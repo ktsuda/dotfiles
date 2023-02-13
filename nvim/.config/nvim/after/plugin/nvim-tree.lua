b6b174d4d664e86976ecbbbaafc202031a3485f4
ktsuda <ktsuda@users.noreply.github.com>
2023-02-14 00:15:39 +0900

    [nvim] Do not use nvim tree callback
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- nvim/.config/nvim/after/plugin/nvim-tree.lua
+++ nvim/.config/nvim/after/plugin/nvim-tree.lua
@@ -1,8 +1,9 @@
 local nvim_tree_status, nvim_tree = pcall(require, 'nvim-tree')
+
 if not nvim_tree_status then
   return
 end
-local tree_cb = require('nvim-tree.config').nvim_tree_callback
+
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 nvim_tree.setup({
@@ -16,11 +17,11 @@ nvim_tree.setup({
     mappings = {
       custom_only = false,
       list = {
-        { key = { 'l', '<CR>', 'o' }, cb = tree_cb('edit') },
+        { key = { '<CR>', 'o' }, action = 'edit' },
+        { key = 'l', action = 'preview' },
         { key = '<C-e>', action = '' },
-        { key = 'h', cb = tree_cb('close_node') },
-        { key = 'v', cb = tree_cb('vsplit') },
-        { key = 'H', action = '' },
+        { key = 'h', action = 'close_node' },
+        { key = 'v', action = 'vsplit' },
         { key = 'I', action = 'toggle_dotfiles' },
         { key = 'H', action = 'toggle_ignored' },
         { key = 'y', action = 'copy_name' },
