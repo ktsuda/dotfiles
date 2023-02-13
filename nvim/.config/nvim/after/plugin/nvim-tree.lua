b98ff5f5c8df06d591095edde4cadde1871cb13b
ktsuda <ktsuda@users.noreply.github.com>
2023-02-14 01:45:28 +0900

    [nvim] Update nvim-tree.lua
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- nvim/.config/nvim/after/plugin/nvim-tree.lua
+++ nvim/.config/nvim/after/plugin/nvim-tree.lua
@@ -8,6 +8,7 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 nvim_tree.setup({
   update_cwd = true,
+  sync_root_with_cwd = true,
   update_focused_file = {
     enable = true,
     update_cwd = true,
