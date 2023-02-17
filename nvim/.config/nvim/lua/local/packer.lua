a8c4ed22ef6223e777acd912644356e9f55a2b98
ktsuda <ktsuda@users.noreply.github.com>
2023-02-18 07:45:22 +0900

    [nvim] Delete custom highlights
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- nvim/.config/nvim/lua/local/packer.lua
+++ nvim/.config/nvim/lua/local/packer.lua
@@ -63,10 +63,6 @@ packer.startup(function(use)
       config = function()
         vim.g.neosolarized_underline = 0
         vim.cmd.colorscheme('NeoSolarized')
-        vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-        vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-        vim.api.nvim_set_hl(0, 'NonText', { bg = 'none' })
-        vim.api.nvim_set_hl(0, 'LineNr', { bg = 'none' })
       end,
     },
     {
