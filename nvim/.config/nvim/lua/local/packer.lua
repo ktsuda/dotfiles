81682e964652423da0076013d443f8876efa248a
ktsuda <ktsuda@users.noreply.github.com>
2023-03-01 11:27:50 +0900

    [alacritty] Make it transparent
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- nvim/.config/nvim/lua/local/packer.lua
+++ nvim/.config/nvim/lua/local/packer.lua
@@ -62,6 +62,8 @@ packer.startup(function(use)
       'ellisonleao/gruvbox.nvim',
       config = function()
         vim.cmd.colorscheme('gruvbox')
+        vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
       end,
     },
     {
