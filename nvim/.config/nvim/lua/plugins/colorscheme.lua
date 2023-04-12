dc53d6e283bac6e56155061104b7765b9048e6f5
ktsuda <ktsuda@users.noreply.github.com>
2023-04-12 12:23:21 +0900

    add colorscheme
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,8 @@
+return {
+	'ellisonleao/gruvbox.nvim',
+	config = function()
+		vim.cmd.colorscheme('gruvbox')
+		vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+		vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+	end,
+}
