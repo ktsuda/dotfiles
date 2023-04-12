3591a71383023123ed4f6add4c9b9e4cf57b71a4
ktsuda <ktsuda@users.noreply.github.com>
2023-04-12 17:23:50 +0900

    Add completion.lua
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

---
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,93 @@
+return {
+  {
+    'L3MON4D3/LuaSnip',
+    event = 'InsertEnter',
+    dependencies = {
+      'rafamadriz/friendly-snippets',
+      config = function()
+        require('luasnip.loaders.from_vscode').lazy_load()
+      end,
+    },
+    opts = {
+      history = true,
+      delete_check_events = 'TextChanged',
+    },
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter', 
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-nvim-lua',
+      'saadparwaiz1/cmp_luasnip',
+      'L3MON4D3/LuaSnip',
+    },
+    opts = function()
+      local cmp = require('cmp')
+      local luasnip = require('luasnip')
+      local custom_cmp = {
+        next_expand_jump = function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end,
+        prev_expand_jump = function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end,
+        complete_common = function(fallback)
+          if cmp.visible() then
+            return cmp.complete_common_string()
+          end
+          fallback()
+        end,
+      }
+      return {
+        mapping = {
+          ['<C-n>'] = cmp.mapping(custom_cmp.next_expand_jump, { 'i', 's' }),
+          ['<C-p>'] = cmp.mapping(custom_cmp.prev_expand_jump, { 'i', 's' }),
+          ['<C-g>'] = cmp.mapping.abort(),
+          ['<C-Space>'] = cmp.mapping(custom_cmp.complete_common, { 'i', 's' }),
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<CR>'] = cmp.mapping.confirm({ select = false }),
+        },
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = 'nvim_lua' },
+          { name = 'luasnip' },
+          { name = 'buffer' },
+          { name = 'path' },
+        },
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        sorting = {
+          comparators = {
+            cmp.config.compare.offset,
+            cmp.config.compare.exact,
+            cmp.config.compare.sort_text,
+            cmp.config.compare.score,
+            cmp.config.compare.recently_used,
+            cmp.config.compare.kind,
+            cmp.config.compare.length,
+            cmp.config.compare.order,
+          },
+        },
+      }
+    end
+  },
+}
