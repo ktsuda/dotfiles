6b1e5c9e91e7b05082a621e67e2b423e48e6b5af
ktsuda <ktsuda@users.noreply.github.com>
2023-04-12 16:36:06 +0900

    Add skkeleton
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

---
+++ nvim/.config/nvim/lua/plugins/input.lua
@@ -0,0 +1,18 @@
+return {
+  'vim-skk/skkeleton',
+  keys = {
+    { '<C-j>', '<Plug>(skkeleton-toggle)', mode = { 'i', 'c' }, desc = 'SKK' },
+  },
+  dependencies = {
+    { 'vim-denops/denops.vim' },
+  },
+  config = function()
+    vim.keymap.set({ 'i', 'c' }, '<C-j>', '<Plug>(skkeleton-toggle)', { noremap = false })
+    vim.cmd [[
+      call skkeleton#config({
+        \   'eggLikeNewline': v:true,
+        \   'globalJisyo': expand('~/.cache/skk/SKK-JISYO.L'),
+        \ })
+    ]]
+  end,
+}
