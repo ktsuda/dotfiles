85bea5f8bb399e1d09afa2d8dd4ba5fde26b45c4
ktsuda <ktsuda@users.noreply.github.com>
2023-04-16 08:44:36 +0900

    [nvim] Update decoration.lua
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- nvim/.config/nvim/lua/plugins/decoration.lua
+++ nvim/.config/nvim/lua/plugins/decoration.lua
@@ -17,29 +17,12 @@ return {
           theme = 'gruvbox_dark',
           icons_enabled = true,
         },
-        sections = {
-          lualine_b = {
-            { 'branch' },
-          },
-          lualine_c = {
-            {
-              'diff',
-              colored = true,
-              diff_color = {
-                added = 'DiffAdd',
-                modified = 'DiffChange',
-                removed = 'DiffDelete',
-              },
-            },
-            { 'diagnostics' },
-            { 'filename' },
-          },
-        },
         extensions = {
           'quickfix',
           'fugitive',
           'nvim-tree',
           'fzf',
+          'lazy',
         },
       },
     },
