680c96540cd62393105206a5d8700a613b362af5
ktsuda <ktsuda@users.noreply.github.com>
2023-04-16 07:25:11 +0900

    [nvim] Introduce FTerm
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- nvim/.config/nvim/lua/plugins/terminal.lua
+++ nvim/.config/nvim/lua/plugins/terminal.lua
@@ -1,10 +1,49 @@
 return {
-  'voldikss/vim-floaterm',
-  cmd = { 'FloatermToggle', 'FloatermNew' },
-  keys = {
-    { '<leader>tt', '<cmd>FloatermToggle<cr>', mode = 'n', desc = 'terminal' },
-    { '<leader>tt', '<C-\\><C-n>FloatermToggle<cr>', mode = 't', desc = 'terminal' },
-    { '<leader>ts', '<cmd>FloatermNew tig status<cr>', desc = 'tig status' },
-    { '<leader>gl', '<cmd>FloatermNew --height=1.0 tig %<cr>', desc = 'git log -p' },
-  },
+    {
+        'numToStr/FTerm.nvim',
+        keys = {
+            {
+                '<leader>tt',
+                '<cmd>lua require("FTerm").toggle()<cr>',
+                mode = 'n',
+                desc = 'terminal',
+            },
+            {
+                '<leader>tt',
+                '<C-\\><C-n><cmd>lua require("FTerm").toggle()<cr>',
+                mode = 't',
+                desc = 'terminal',
+            },
+            { '<leader>ts', '<cmd>FTermTigStatus<cr>', desc = 'tig status' },
+            { '<leader>gl', '<cmd>FTermGitLogP %<cr>', desc = 'git log -p' },
+        },
+        cmd = { 'FTermTigStatus', 'FTermGitLogP' },
+        opts = {
+            border = 'double',
+            dimensions = {
+                height = 0.9,
+                width = 0.9,
+            },
+        },
+        config = function(_, opts)
+            local fterm = require('FTerm')
+            fterm.setup(opts)
+            local border = opts.border or 'double'
+            local dimensions = opts.dimensions or {}
+            vim.api.nvim_create_user_command('FTermTigStatus', function()
+              fterm:new({
+                cmd = 'tig status',
+                border = border,
+                dimensions = dimensions,
+              }):toggle()
+            end, { bang = true })
+            vim.api.nvim_create_user_command('FTermGitLogP', function(path)
+              fterm:new({
+                cmd = 'tig ' .. vim.fn.expand(path.args),
+                border = border,
+                dimensions = dimensions,
+              }):toggle()
+            end, { bang = true, nargs = '?' })
+        end,
+    },
 }
