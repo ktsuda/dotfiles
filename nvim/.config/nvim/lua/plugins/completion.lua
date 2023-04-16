34796125536de509fa3d417429a96a85e762a109
ktsuda <ktsuda@users.noreply.github.com>
2023-04-16 16:25:22 +0900

    [nvim] Introduce cmp-cmdline
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -90,4 +90,33 @@ return {
       }
     end
   },
+  {
+    'hrsh7th/cmp-cmdline',
+    dependencies = {
+      'hrsh7th/nvim-cmp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+    },
+    event = { 'CmdlineEnter' },
+    config = function()
+      local cmp = require('cmp')
+      cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' },
+        },
+      })
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' },
+        }, {
+          name = 'cmdline',
+          option = {
+            ignore_cmds = { 'Man', '!' },
+          },
+        })
+      })
+    end,
+  },
 }
