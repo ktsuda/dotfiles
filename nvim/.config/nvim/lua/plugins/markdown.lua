0ff949e8a51beb3c5dfc4b155f2a8de3483158f1
ktsuda <ktsuda@users.noreply.github.com>
2023-04-23 15:42:41 +0900

    [nvim] Revert original key bindings
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,24 +1,15 @@
 return {
-    {
-      'dhruvasagar/vim-table-mode',
-      ft = { 'markdown' },
-      config = function()
-        vim.g.table_mode_corner = '|'
-      end,
-    },
-    {
-      'iamcco/markdown-preview.nvim',
-      cond = function()
+    'iamcco/markdown-preview.nvim',
+    cond = function()
         return vim.fn.executable('npm') == 1
-      end,
-      build = 'cd app && npm install',
-      ft = { 'markdown' },
-      config = function()
+    end,
+    build = 'cd app && npm install',
+    ft = { 'markdown' },
+    config = function()
         vim.g.mkdp_filetypes = { 'markdown' }
         vim.g.mkdp_auto_start = 0
         vim.g.mkdp_auto_close = 1
         vim.g.mkdp_refresh_slow = 0
         vim.keymap.set('n', '<leader>mp', vim.cmd.MarkdownPreview, {})
-      end,
-    },
+    end,
 }
