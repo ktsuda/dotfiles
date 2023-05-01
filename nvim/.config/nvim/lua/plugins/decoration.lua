050b39877c3d9fb09e8fbd7ba364621be843c9a9
ktsuda <ktsuda@users.noreply.github.com>
2023-05-01 16:17:28 +0900

    [nvim] Delete bufferline
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- nvim/.config/nvim/lua/plugins/decoration.lua
+++ nvim/.config/nvim/lua/plugins/decoration.lua
@@ -1,30 +1,20 @@
 return {
-    {
-      'akinsho/bufferline.nvim',
-      event = { 'BufReadPre', 'BufNewFile' },
-      dependencies = {
-        { 'kyazdani42/nvim-web-devicons' },
-      },
-      opts = {},
+  'nvim-lualine/lualine.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  dependencies = {
+    { 'kyazdani42/nvim-web-devicons' },
+  },
+  opts = {
+    options = {
+      theme = 'gruvbox_dark',
+      icons_enabled = true,
     },
-    {
-      'nvim-lualine/lualine.nvim',
-      event = { 'BufReadPre', 'BufNewFile' },
-      dependencies = {
-        { 'kyazdani42/nvim-web-devicons' },
-      },
-      opts = {
-        options = {
-          theme = 'gruvbox_dark',
-          icons_enabled = true,
-        },
-        extensions = {
-          'quickfix',
-          'fugitive',
-          'nvim-tree',
-          'fzf',
-          'lazy',
-        },
-      },
+    extensions = {
+      'quickfix',
+      'fugitive',
+      'nvim-tree',
+      'fzf',
+      'lazy',
     },
-  }
+  },
+}
