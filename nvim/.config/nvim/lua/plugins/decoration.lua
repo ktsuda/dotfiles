6ac46504c71a45d4cd55e68413cac5acf4f7bf5f
ktsuda <ktsuda@users.noreply.github.com>
2023-05-23 12:08:20 +0900

    [nvim] Reenable bufferline
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- nvim/.config/nvim/lua/plugins/decoration.lua
+++ nvim/.config/nvim/lua/plugins/decoration.lua
@@ -1,20 +1,30 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  event = { 'BufReadPre', 'BufNewFile' },
-  dependencies = {
-    { 'kyazdani42/nvim-web-devicons' },
-  },
-  opts = {
-    options = {
-      theme = 'gruvbox_dark',
-      icons_enabled = true,
+  {
+    'akinsho/bufferline.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+      { 'kyazdani42/nvim-web-devicons' },
     },
-    extensions = {
-      'quickfix',
-      'fugitive',
-      'nvim-tree',
-      'fzf',
-      'lazy',
+    opts = {},
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+      { 'kyazdani42/nvim-web-devicons' },
+    },
+    opts = {
+      options = {
+        theme = 'gruvbox_dark',
+        icons_enabled = true,
+      },
+      extensions = {
+        'quickfix',
+        'fugitive',
+        'nvim-tree',
+        'fzf',
+        'lazy',
+      },
     },
   },
 }
