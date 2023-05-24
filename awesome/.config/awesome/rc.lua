d16d99ba21310d33ed99183d61e447ffa4886615
ktsuda <ktsuda@users.noreply.github.com>
2023-05-25 07:10:31 +0900

    [awesome] Delete unnecessary keymaps
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -153,12 +153,6 @@ local taglist_buttons = gears.table.join(
     if client.focus then
       client.focus:toggle_tag(t)
     end
-  end),
-  awful.button({}, 4, function(t)
-    awful.tag.viewnext(t.screen)
-  end),
-  awful.button({}, 5, function(t)
-    awful.tag.viewprev(t.screen)
   end)
 )
 
@@ -369,22 +363,13 @@ globalkeys = gears.table.join(
 )
 
 clientkeys = gears.table.join(
-  awful.key({ modkey }, 'f', function(c)
+  awful.key({ modkey, 'Control' }, 'Return', function(c)
     c.fullscreen = not c.fullscreen
     c:raise()
   end, { description = 'toggle fullscreen', group = 'client' }),
   awful.key({ modkey, 'Shift' }, 'c', function(c)
     c:kill()
   end, { description = 'close', group = 'client' }),
-  awful.key(
-    { modkey, 'Control' },
-    'space',
-    awful.client.floating.toggle,
-    { description = 'toggle floating', group = 'client' }
-  ),
-  awful.key({ modkey, 'Control' }, 'Return', function(c)
-    c:swap(awful.client.getmaster())
-  end, { description = 'move to master', group = 'client' }),
   awful.key({ modkey }, 'o', function(c)
     c:move_to_screen()
   end, { description = 'move to screen', group = 'client' }),
@@ -416,14 +401,6 @@ for i = 1, 9 do
         tag:view_only()
       end
     end, { description = 'view tag #' .. i, group = 'tag' }),
-    -- Toggle tag display.
-    awful.key({ modkey, 'Control' }, '#' .. i + 9, function()
-      local screen = awful.screen.focused()
-      local tag = screen.tags[i]
-      if tag then
-        awful.tag.viewtoggle(tag)
-      end
-    end, { description = 'toggle tag #' .. i, group = 'tag' }),
     -- Move client to tag.
     awful.key({ modkey, 'Shift' }, '#' .. i + 9, function()
       if client.focus then
@@ -432,16 +409,7 @@ for i = 1, 9 do
           client.focus:move_to_tag(tag)
         end
       end
-    end, { description = 'move focused client to tag #' .. i, group = 'tag' }),
-    -- Toggle tag on focused client.
-    awful.key({ modkey, 'Control', 'Shift' }, '#' .. i + 9, function()
-      if client.focus then
-        local tag = client.focus.screen.tags[i]
-        if tag then
-          client.focus:toggle_tag(tag)
-        end
-      end
-    end, { description = 'toggle focused client on tag #' .. i, group = 'tag' })
+    end, { description = 'move focused client to tag #' .. i, group = 'tag' })
   )
 end
 
