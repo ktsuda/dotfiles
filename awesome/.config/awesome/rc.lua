2cac9d953ba1f9f44a7857d82224f133d7b4a6ae
ktsuda <ktsuda@users.noreply.github.com>
2023-07-23 00:08:43 +0900

    [awesome] Update rc.local
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -269,8 +269,8 @@ root.buttons(gears.table.join(
 -- {{{ Key bindings
 globalkeys = gears.table.join(
   awful.key({ modkey }, 's', hotkeys_popup.show_help, { description = 'show help', group = 'awesome' }),
-  awful.key({ modkey }, 'Left', awful.tag.viewprev, { description = 'view previous', group = 'tag' }),
-  awful.key({ modkey }, 'Right', awful.tag.viewnext, { description = 'view next', group = 'tag' }),
+  awful.key({ modkey }, 'h', awful.tag.viewprev, { description = 'view previous', group = 'tag' }),
+  awful.key({ modkey }, 'l', awful.tag.viewnext, { description = 'view next', group = 'tag' }),
   awful.key({ modkey }, 'Escape', awful.tag.history.restore, { description = 'go back', group = 'tag' }),
   awful.key({ modkey }, 'j', function()
     awful.client.focus.byidx(1)
@@ -289,10 +289,10 @@ globalkeys = gears.table.join(
   awful.key({ modkey, 'Shift' }, 'k', function()
     awful.client.swap.byidx(-1)
   end, { description = 'swap with previous client by index', group = 'client' }),
-  awful.key({ modkey }, 'l', function()
+  awful.key({ modkey, 'Control' }, 'l', function()
     awful.screen.focus_relative(1)
   end, { description = 'focus the next screen', group = 'screen' }),
-  awful.key({ modkey }, 'h', function()
+  awful.key({ modkey, 'Control' }, 'h', function()
     awful.screen.focus_relative(-1)
   end, { description = 'focus the previous screen', group = 'screen' }),
   awful.key({ modkey }, 'u', awful.client.urgent.jumpto, { description = 'jump to urgent client', group = 'client' }),
@@ -312,18 +312,18 @@ globalkeys = gears.table.join(
   end, { description = 'Brave Browser', group = 'launcher' }),
   awful.key({ modkey, 'Control' }, 'r', awesome.restart, { description = 'reload awesome', group = 'awesome' }),
   awful.key({ modkey, 'Shift' }, 'q', awesome.quit, { description = 'quit awesome', group = 'awesome' }),
-  awful.key({ modkey, 'Control' }, 'l', function()
+  awful.key({ modkey, 'Shift' }, 'l', function()
     awful.tag.incmwfact(0.05)
   end, { description = 'increase master width factor', group = 'layout' }),
-  awful.key({ modkey, 'Control' }, 'h', function()
+  awful.key({ modkey, 'Shift' }, 'h', function()
     awful.tag.incmwfact(-0.05)
   end, { description = 'decrease master width factor', group = 'layout' }),
-  awful.key({ modkey, 'Shift' }, 'h', function()
-    awful.tag.incnmaster(1, nil, true)
-  end, { description = 'increase the number of master clients', group = 'layout' }),
-  awful.key({ modkey, 'Shift' }, 'l', function()
-    awful.tag.incnmaster(-1, nil, true)
-  end, { description = 'decrease the number of master clients', group = 'layout' }),
+  -- awful.key({ modkey, 'Shift' }, 'h', function()
+  --   awful.tag.incnmaster(1, nil, true)
+  -- end, { description = 'increase the number of master clients', group = 'layout' }),
+  -- awful.key({ modkey, 'Shift' }, 'l', function()
+  --   awful.tag.incnmaster(-1, nil, true)
+  -- end, { description = 'decrease the number of master clients', group = 'layout' }),
   awful.key({ modkey }, 'space', function()
     awful.layout.inc(1)
   end, { description = 'select next', group = 'layout' }),
