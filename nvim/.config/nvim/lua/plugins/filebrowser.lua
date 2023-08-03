bf3d67448fd38be2cb9342dae6398002a7f28939
ktsuda <ktsuda@users.noreply.github.com>
2023-08-03 12:58:55 +0900

    [nvim] Resolve deprecation warning
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- nvim/.config/nvim/lua/plugins/filebrowser.lua
+++ nvim/.config/nvim/lua/plugins/filebrowser.lua
@@ -19,22 +19,23 @@ return {
     },
     view = {
       width = 48,
-      mappings = {
-        custom_only = false,
-        list = {
-          { key = { '<CR>', 'o' }, action = 'edit' },
-          { key = 'l', action = 'preview' },
-          { key = '<C-e>', action = '' },
-          { key = 'h', action = 'close_node' },
-          { key = 'v', action = 'vsplit' },
-          { key = 'I', action = 'toggle_dotfiles' },
-          { key = 'H', action = 'toggle_ignored' },
-          { key = 'y', action = 'copy_name' },
-          { key = 'gy', action = 'copy_path' },
-          { key = 'Y', action = 'copy_absolute_path' },
-        },
-      },
     },
+    on_attach = function(bufnr)
+      local api = require('nvim-tree.api')
+      local function keymap_opts(desc)
+        return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+      vim.keymap.set('n', '<CR>', api.node.open.edit, keymap_opts('Open'))
+      vim.keymap.set('n', 'o', api.node.open.edit, keymap_opts('Open'))
+      vim.keymap.set('n', 'l', api.node.open.preview, keymap_opts('Open Preview'))
+      vim.keymap.set('n', 'h', api.node.navigate.parent_close, keymap_opts('Close Directory'))
+      vim.keymap.set('n', 'v', api.node.open.vertical, keymap_opts('Open: Vertical Split'))
+      vim.keymap.set('n', 'I', api.tree.toggle_hidden_filter, keymap_opts('Toggle Dotfiles'))
+      vim.keymap.set('n', 'H', api.tree.toggle_gitignore_filter, keymap_opts('Toggle Git Ignore'))
+      vim.keymap.set('n', 'y', api.fs.copy.filename, keymap_opts('Copy Name'))
+      vim.keymap.set('n', 'gy', api.fs.copy.relative_path, keymap_opts('Copy Relative Path'))
+      vim.keymap.set('n', 'Y', api.fs.copy.absolute_path, keymap_opts('Copy Absolute Path'))
+    end,
   },
   config = function(_, opts)
     vim.g.loaded_netrw = 1
