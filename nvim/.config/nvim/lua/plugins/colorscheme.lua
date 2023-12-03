f97a679692eb4f020f7f273cbb848d99418c4940
ktsuda <ktsuda@users.noreply.github.com>
2023-12-03 20:43:51 +0900

    [nvim] Delete transparent.nvim
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,5 @@
 return {
   {
-    'xiyaowong/transparent.nvim',
-    opts = function(_, opts)
-      return vim.tbl_extend('force', {
-        extra_groups = {
-          'NormalFloat',
-          'FloatBorder',
-          'FloatTitle',
-          'Pmenu',
-          'WinSeparator',
-        },
-      }, opts or {})
-    end,
-  },
-  {
     'navarasu/onedark.nvim',
     priority = 1000,
     config = function()
