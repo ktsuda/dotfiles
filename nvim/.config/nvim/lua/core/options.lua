b8b88f64da7158fa60b3aff6f1b9e502c6abf2b1
ktsuda <ktsuda@users.noreply.github.com>
2023-12-12 10:14:01 +0900

    [nvim] Update options
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -1,34 +1,45 @@
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.o.softtabstop = 2
-vim.o.expandtab = true
-vim.wo.number = true
-vim.wo.relativenumber = true
-vim.o.smartindent = true
-vim.o.laststatus = 2
-vim.wo.signcolumn = 'yes'
-vim.o.foldmethod = 'marker'
-vim.o.hlsearch = false
-vim.o.incsearch = true
-vim.o.termguicolors = true
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.updatetime = 50
-vim.o.guicursor = ''
-vim.o.breakindent = true
-vim.o.colorcolumn = '80'
-vim.o.background = 'dark'
-vim.o.completeopt = 'menu,menuone,noselect'
-vim.o.mouse = 'a'
-vim.o.formatoptions = 'jcroqlmM'
-vim.o.diffopt = 'internal,filler,closeoff,vertical'
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.smartindent = true
+vim.opt.laststatus = 2
+vim.opt.signcolumn = 'yes'
+vim.opt.foldmethod = 'marker'
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.updatetime = 50
+vim.opt.guicursor = ''
+vim.opt.breakindent = true
+vim.opt.colorcolumn = '80'
+vim.opt.background = 'dark'
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+vim.opt.mouse = 'a'
+vim.opt.formatoptions:append({ m = true, M = true })
+vim.opt.diffopt:append('vertical')
+vim.opt.spell = true
+vim.opt.spelllang:append('cjk')
+vim.opt.whichwrap:append({
+  ['<'] = true,
+  ['>'] = true,
+  ['['] = true,
+  [']'] = true,
+  h = true,
+  l = true,
+})
+vim.opt.autoread = true
 
 if vim.fn.has('mac') == 1 then
-  vim.o.clipboard = 'unnamed'
+  vim.opt.clipboard = 'unnamed'
 elseif vim.fn.has('win64') == 1 then
-  vim.o.clipboard = 'unnamed'
+  vim.opt.clipboard = 'unnamed'
 elseif vim.fn.has('win32') == 1 then
-  vim.o.clipboard = 'unnamed'
+  vim.opt.clipboard = 'unnamed'
 else
-  vim.o.clipboard = 'unnamedplus' -- install xsel or xclip
+  vim.opt.clipboard = 'unnamedplus' -- install xsel or xclip
 end
