227585afbcee317989c2c1eec82b2474cdce8572
ktsuda <ktsuda@users.noreply.github.com>
2024-01-30 14:12:06 +0900

    Fix script to install bat theme
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- bat/.config/bat/themes/install.sh
+++ bat/.config/bat/themes/install.sh
@@ -3,6 +3,8 @@
 BAT_THEMES_DIR="$(command bat --config-dir)/themes"
 BAT_THEME_CACHE="$(command bat --cache-dir)/themes.bin"
 
-if [ -n "$(find "$BAT_THEMES_DIR" -name '*.tmTheme' -newer "$BAT_THEME_CACHE")" ]; then
+if [ ! -e "$BAT_THEME_CACHE" ]; then
+    command bat cache --build
+elif [ -n "$(find "$BAT_THEMES_DIR" -name '*.tmTheme' -newer "$BAT_THEME_CACHE")" ]; then
     command bat cache --build
 fi
