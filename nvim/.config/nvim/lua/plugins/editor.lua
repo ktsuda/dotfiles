2aeb55c964eecc1277c4d5e28f6c038d303d5230
ktsuda <ktsuda@users.noreply.github.com>
2024-02-17 19:12:47 +0900

    [nvim] Update comment.nvim config
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -15,9 +15,30 @@ return {
     },
   },
   {
-    -- 'gc' to comment visual regions/lines
     'numToStr/Comment.nvim',
-    opts = {},
+    keys = {
+      { '<leader>cc', '<Plug>(comment_toggle_linewise)', mode = { 'v' }, desc = '[c]omment [c]urrent line' },
+      { '<leader>cb', '<Plug>(comment_toggle_blockwise)', mode = { 'v' }, desc = '[c]omment [b]lockwise' },
+      { '<leader>cc', '<Plug>(comment_toggle_linewise_current)', mode = { 'n' }, desc = '[c]omment [c]urrent line' },
+      { '<leader>cb', '<Plug>(comment_toggle_blockwise_current)', mode = { 'n' }, desc = '[c]omment [b]lockwise' },
+    },
+    opts = {
+      padding = true,
+      sticky = true,
+      ignore = '^$',
+      toggler = {
+        line = '<leader>cc',
+        block = '<leader>bc',
+      },
+      opleader = {
+        line = '<leader>cc',
+        block = '<leader>bc',
+      },
+      mappings = {
+        basic = true,
+        extra = false,
+      },
+    },
   },
   {
     'windwp/nvim-ts-autotag',
