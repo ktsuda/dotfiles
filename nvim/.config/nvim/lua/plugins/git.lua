3d3731bdeb530a0fc02796b9f5779d100624b304
ktsuda <ktsuda@users.noreply.github.com>
2024-03-07 11:48:40 +0900

    [nvim] Update git.lua
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -8,7 +8,7 @@ return {
     },
     cmd = { 'G', 'Git', 'Gdiff', 'GBrowse' },
     keys = {
-      { '<leader>gs', '<cmd>G<cr>', desc = '[g]it [s]tatus' },
+      { '<leader>gs', '<cmd>0G<cr>', desc = '[g]it [s]tatus' },
       { '<leader>gd', '<cmd>Gdiff<cr>', desc = '[g]it [d]iff' },
       { '<leader>br', '<cmd>GBrowse<cr>', desc = '[b]rowse [r]epo' },
     },
