7d5975658a3b3c3e85a41b0fff9a17581df03589
ktsuda <ktsuda@users.noreply.github.com>
2024-03-20 13:53:45 +0900

    [nvim] Revert skkeleton
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- nvim/.config/nvim/lua/plugins/inputmethod.lua
+++ nvim/.config/nvim/lua/plugins/inputmethod.lua
@@ -1,5 +1,6 @@
 return {
-  'vim-skk/skkeleton',
+  'ktsuda/skkeleton',
+  branch = 'revert-to-old-saver',
   keys = {
     { '<C-j>', '<Plug>(skkeleton-toggle)', mode = { 'i', 'c' }, desc = 'SKK' },
   },
