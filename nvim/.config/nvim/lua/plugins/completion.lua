0573aaed80bfeba093914254cdf6f6ad76deae79
ktsuda <ktsuda@users.noreply.github.com>
2024-09-20 09:05:12 +0900

    [nvim] Update completion.lua
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -10,7 +10,7 @@ return {
         'hrsh7th/cmp-cmdline',
         config = function()
           local cmp = require('cmp')
-          cmp.setup.cmdline('/', {
+          cmp.setup.cmdline({ '/', '?' }, {
             mapping = cmp.mapping.preset.cmdline(),
             sources = {
               { name = 'buffer' },
