3e05e28998696f122fa09e3499980e0b5827e32e
ktsuda <ktsuda@users.noreply.github.com>
2024-12-31 15:50:09 +0900

    [nvim] Add ftplugin
    Signed-off-by: ktsuda <ktsuda@users.noreply.github.com>

---
+++ nvim-tiny/.config/nvim/after/ftplugin/make.lua
@@ -0,0 +1,6 @@
+vim.opt_local.textwidth = 78
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.expandtab = false
+vim.opt.formatoptions:append({ m = true, M = true, t = false })
