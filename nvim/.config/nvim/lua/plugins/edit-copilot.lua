4836dcaffa36c1f9bb0e04b9881d57bff314afe9
ktsuda <ktsuda@users.noreply.github.com>
2025-08-12 07:29:54 +0900

    [nvim] Update keymaps for copilot

--- nvim/.config/nvim/lua/plugins/edit-copilot.lua
+++ nvim/.config/nvim/lua/plugins/edit-copilot.lua
@@ -27,8 +27,8 @@ return {
         replace_keycodes = false,
         desc = 'Copilot: Accept the current suggestion',
       })
-      vim.keymap.set('i', '<C-j>', '<Plug>(copilot-next)', { desc = 'Copilot: Move to the next suggestion' })
-      vim.keymap.set('i', '<C-k>', '<Plug>(copilot-previous)', { desc = 'Copilot: Move to the previous suggestion' })
+      vim.keymap.set('i', '<C-n>', '<Plug>(copilot-next)', { desc = 'Copilot: Move to the next suggestion' })
+      vim.keymap.set('i', '<C-p>', '<Plug>(copilot-previous)', { desc = 'Copilot: Move to the previous suggestion' })
       vim.keymap.set('i', '<C-o>', '<Plug>(copilot-dismiss)', { desc = 'Copilot: Dismiss the current suggestion' })
       vim.keymap.set('i', '<C-s>', '<Plug>(copilot-suggest)', { desc = 'Copilot: Suggest a new completion' })
       vim.g.copilot_no_tab_map = true
