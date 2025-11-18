2ae2b6493c079a5ff3e3d59faeaa9f4db3960f38
ktsuda <ktsuda@users.noreply.github.com>
2025-11-18 16:38:41 +0900

    [nvim] Add keymaps for tab

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,9 +1,19 @@
 vim.keymap.set({ 'n', 'v', 'x' }, '<Space>', '<Nop>', { silent = true }) -- nowait
+
+-- pack
+vim.keymap.set('n', '<leader>pu', vim.pack.update, { desc = 'Update all plugins' })
+
+-- quicklist
 vim.keymap.set('n', '[q', vim.cmd.cprevious, { desc = 'Previous quickfix' })
 vim.keymap.set('n', ']q', vim.cmd.cnext, { desc = 'Next quickfix' })
+
+-- buffer
 vim.keymap.set('n', '<C-h>', vim.cmd.bprev, { desc = 'Buffer: Previous buffer' })
 vim.keymap.set('n', '<C-l>', vim.cmd.bnext, { desc = 'Buffer: Next buffer' })
-vim.keymap.set('n', '<leader>pu', vim.pack.update, { desc = 'Update all plugins' })
+
+-- tab
+vim.keymap.set('n', '[t', vim.cmd.tabprevious, { desc = 'Tab prev' })
+vim.keymap.set('n', ']t', vim.cmd.tabnext, { desc = 'Tab next' })
 
 -- terminal
 vim.keymap.set('n', '<leader>tt', '<cmd>split term://zsh<cr>', { desc = 'Open terminal' })
