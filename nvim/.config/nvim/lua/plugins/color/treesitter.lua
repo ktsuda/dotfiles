b4055be0e9858232487219fb0cbf3884212c48d1
ktsuda <ktsuda@users.noreply.github.com>
2025-11-24 14:32:58 +0900

    [nvim] Update treesitter's configurations

--- nvim/.config/nvim/lua/plugins/color/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/color/treesitter.lua
@@ -1,50 +1,75 @@
-local function load()
-  vim.pack.add({
-    { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
-  })
-
-  -- pcall(require('nvim-treesitter.install').update({ with_sync = true }))
-
-  require('nvim-treesitter.configs').setup({
-    ensure_installed = {
-      'c',
-      'cmake',
-      'cpp',
-      'diff',
-      'dockerfile',
-      -- 'go',
-      'javascript',
-      'json',
-      -- 'latex',
-      'lua',
-      'make',
-      'markdown',
-      'python',
-      -- 'ruby',
-      'tsx',
-      'typescript',
-      'vim',
-      'vimdoc',
+vim.pack.add({
+  {
+    src = 'https://github.com/nvim-treesitter/nvim-treesitter',
+    version = 'main',
+    data = {
+      on_changed = function(event)
+        if event.data.kind == 'install' and event.data.kind == 'update' then
+          pcall(require('nvim-treesitter').update())
+        end
+      end,
     },
-    sync_install = false,
-    auto_install = true,
-    ignore_install = {},
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    modules = {},
-  })
-end
+  },
+}, { load = false })
 
-local group = vim.api.nvim_create_augroup('my.treesitter', {})
+local group = vim.api.nvim_create_augroup('my.treesitter', { clear = true })
 
-local cmd = {
-  group = group,
-  once = true,
-  callback = load,
+vim.api.nvim_create_autocmd('PackChanged', {
+  callback = function(event)
+    local spec = event.data.spec
+
+    if spec.data and spec.data.on_changed then
+      spec.data.on_changed(event)
+    end
+  end,
+})
+
+local pattern = {
+  'lua',
+  'c',
+  'cpp',
+  'markdown',
+  'go',
+  'python',
+  'json',
 }
 
-local events = { 'BufReadPre', 'BufNewFile' }
+vim.api.nvim_create_autocmd('FileType', {
+  group = group,
+  pattern = pattern,
+  callback = function(event)
+    local ts_status, ts = pcall(require, 'nvim-treesitter')
+    if not ts_status then
+      return
+    end
 
-vim.api.nvim_create_autocmd(events, cmd)
+    ts.setup({
+      install_dir = vim.fs.joinpath(vim.fn.stdpath('data'), '/site'),
+    })
+
+    local lang = event.match
+    -- local already_installed = vim.api.nvim_get_runtime_file(('parser/%s.so'):format(lang), true)
+    local already_installed = ts.get_installed()
+    -- if not next(already_installed) then
+    if not vim.tbl_contains(already_installed, lang) then
+      ts.install(lang, { force = false, summary = true })
+    end
+
+    local ok, _ = pcall(vim.treesitter.start, event.buf, lang)
+    if not ok then
+      return
+    end
+  end,
+})
+
+-- local cmd = {
+--   group = group,
+--   once = true,
+--   callback = function()
+--     vim.cmd([[:packadd 'nvim-treesitreesitter tter']])
+--   end,
+-- }
+--
+-- local events = { 'BufReadPre', 'BufNewFile' }
+--
+-- vim.api.nvim_create_autocmd(events, cmd)
