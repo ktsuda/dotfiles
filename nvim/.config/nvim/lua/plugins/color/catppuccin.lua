1490efd0d0a9323ee4d1c8df64a6a40052b9c6e0
ktsuda <ktsuda@users.noreply.github.com>
2025-11-29 13:05:14 +0900

    [nvim] Update colorscheme

--- nvim/.config/nvim/lua/plugins/color/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/color/catppuccin.lua
@@ -2,4 +2,16 @@ vim.pack.add({
   { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' },
 })
 
-vim.cmd.colorscheme('catppuccin')
+require('catppuccin').setup({
+  flavour = 'mocha',
+  color_overrides = {
+    mocha = {
+      base = '#12121a',
+      mantle = '#12121a',
+      crust = '#12121a',
+    },
+  },
+  transparent_background = true,
+})
+
+vim.cmd.colorscheme('catppuccin-mocha')
