31a1d78923ed81b096e0aa4ea5d24fc6e306d84e
ktsuda <ktsuda@users.noreply.github.com>
2025-11-29 10:55:06 +0900

    [xmonad] Change the terminal

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -20,7 +20,7 @@ import qualified Data.Map        as M
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
 --
-myTerminal      = "~/.cargo/bin/alacritty"
+myTerminal      = "/home/linuxbrew/.linuxbrew/bin/wezterm"
 
 -- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
