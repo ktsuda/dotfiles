356fad0b118bf39298a96fa1d6582850c3eb52b0
ktsuda <ktsuda@users.noreply.github.com>
2026-01-19 11:52:09 +0900

    [xmonad] Update border colors

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -55,8 +55,8 @@ myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
 
 -- Border colors for unfocused and focused windows, respectively.
 --
-myNormalBorderColor  = "#dddddd"
-myFocusedBorderColor = "#008888"
+myNormalBorderColor  = "#45475a" -- surface_1
+myFocusedBorderColor = "#cba6f7" -- mauve
 
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
