5311f20422a1819cab1799b6e61252eac4220f8c
ktsuda <ktsuda@users.noreply.github.com>
2026-01-24 07:10:46 +0900

    [nvim] Fold with treesitter

--- nvim-minimal/.config/nvim/after/plugin/treesitter.lua
+++ nvim-minimal/.config/nvim/after/plugin/treesitter.lua
@@ -38,8 +38,7 @@ vim.api.nvim_create_autocmd('FileType', {
       return
     end
 
-    -- vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-    -- vim.wo[0][0].foldmethod = 'expr'
-    -- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    vim.wo[0][0].foldmethod = 'expr'
   end,
 })
