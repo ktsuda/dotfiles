a1cda748dc7d981cebea2c1627e4f03448ef4a5f
ktsuda <ktsuda@users.noreply.github.com>
2026-03-08 12:54:31 +0900

    [nvim] Update oil config

--- nvim/.config/nvim/after/plugin/oil.lua
+++ nvim/.config/nvim/after/plugin/oil.lua
@@ -1,2 +1,1 @@
--- vim.keymap.set('n', '<C-e>', function() require('oil').toggle_float() end, { desc = 'Open Oil' })
-vim.keymap.set('n', '<C-e>', function() require('oil').open() end, { desc = 'Open Oil' })
+vim.keymap.set('n', '<C-e>', function() require('oil').toggle_float() end, { desc = 'Open Oil' })
