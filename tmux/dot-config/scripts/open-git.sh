c9d11dec6523724f2e21a9a7efe0d47128da8ecd
ktsuda <ktsuda@users.noreply.github.com>
2026-05-15 08:09:01 +0900

    Refactor git URL parsing for SSH and HTTPS
    Update remote URL conversion logic to handle additional git host formats.
    Replace domain matching patterns to catch broader host names, and add
    handling for SSH URLs with non-standard domains that use colon notation
    before .com. Simplify HTTPS URL detection and remove .git suffix when
    present.

--- tmux/dot-config/scripts/open-git.sh
+++ tmux/dot-config/scripts/open-git.sh
@@ -4,11 +4,17 @@ dir=$(tmux run-shell 'echo "#{pane_current_path}"')
 cd $dir
 url=$(git remote get-url origin)
 
-if [[ $url == *github.com* ]] || [[ $url == *gitlab.com* ]] || [[ $url == *gitlab.eurecom.fr* ]]; then
-  if [[ $url == git@* ]]; then
+if [[ $url == *github* ]] || [[ $url == *gitlab* ]] || [[ $url == *gitlab.eurecom.fr* ]]; then
+  if [[ $url == http* ]]; then
+    :
+  elif [[ $url == git@*.com:* ]]; then
     url="${url#git@}"
     url="${url/:/\/}"
     url="https://$url"
+  elif [[ $url == *:* ]]; then
+    url="${url/:/.com\/}"
+    url="https://$url"
+    url="${url/.git/}"
   fi
   case ${OSTYPE} in
     darwin*)
