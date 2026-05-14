d8ae72d7f09bc6e428a02df48b757d18e44815a8
ktsuda <ktsuda@users.noreply.github.com>
2026-05-14 11:31:23 +0900

    Refactor lualine and tmux theme config
    Update lualine setup to disable statusline for oil and snacks
    filetypes. Comment out nvim-web-devicons dependency and reorganize
    catppuccin mocha color variables for consistency. Update color
    references to use shortened variable names and adjust message styling
    from blue to peach.

--- nvim/dot-config/nvim/lua/plugin/lualine.lua
+++ nvim/dot-config/nvim/lua/plugin/lualine.lua
@@ -1,6 +1,14 @@
 vim.pack.add({
   { src = 'https://github.com/nvim-lualine/lualine.nvim' },
-  { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
+  -- { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
 })
 
-require('lualine').setup()
+require('lualine').setup({
+  options = {
+    disabled_filetypes = {
+      'oil',
+      'snacks_picker_input',
+      'snacks_dashboard',
+    },
+  },
+})
--- nvim/dot-config/nvim/lua/plugin/oil.lua
+++ nvim/dot-config/nvim/lua/plugin/oil.lua
@@ -1,5 +1,6 @@
 vim.pack.add({
   { src = 'https://github.com/stevearc/oil.nvim' },
+  -- { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
 })
 
 require('oil').setup({
