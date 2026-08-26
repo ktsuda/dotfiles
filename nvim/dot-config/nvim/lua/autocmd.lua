cd54ea0f2406625a23089266a7b651822c1d2335
ktsuda <ktsuda@users.noreply.github.com>
2026-08-26 18:46:46 +0900

    Fix deprecated vim.highlight.on_yank API call
    Replace vim.highlight.on_yank with vim.hl.hl_op to use the current
    Neovim highlight API, addressing deprecation warning in newer versions.

--- nvim/dot-config/nvim/lua/autocmd.lua
+++ nvim/dot-config/nvim/lua/autocmd.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	pattern = "*",
 	desc = "highlight selection on yank",
 	callback = function()
-		vim.highlight.on_yank({ timeout = 200, visual = true })
+		vim.hl.hl_op({ timeout = 200, visual = true })
 	end,
 })
 
