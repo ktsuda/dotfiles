32bddf7ccf9e7ff2aa01e7ee6f0fa677752476ca
ktsuda <ktsuda@users.noreply.github.com>
2026-09-20 14:35:26 +0900

    Configure autoclose.nvim with single quote and auto-indent settings
    Customize the autoclose plugin to escape single quotes instead of pairing them, and enable automatic indentation when closing brackets.

--- nvim/dot-config/nvim/lua/plugin/autoclose.lua
+++ nvim/dot-config/nvim/lua/plugin/autoclose.lua
@@ -2,4 +2,11 @@ vim.pack.add({
   { src = "https://github.com/m4xshen/autoclose.nvim" },
 })
 
-require("autoclose").setup()
+require("autoclose").setup({
+  keys = {
+    ["'"] = { escape = true, close = false, pair = "''" },
+  },
+  options = {
+    auto_indent = true,
+  }
+})
